40234ca6e25922fb66d8f9ecb7a3c3a7f5e4c4b9
Sidharth Sudarsan <git@sidharth.me>
2022-03-21 11:11:45 -0400

    express init

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const app = express();
-
-app.get("/", (req, res) => {
-	res.send("nicely");
-});
-
-app.listen(3000, () => console.log("Listening nicely on port 3000"));
